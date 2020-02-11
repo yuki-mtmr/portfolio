35d7752762019b75a851262aa1c91e2ea67829e0
yuki-mtmr <yuki.matsumori.exam@gmail.com>
2020-02-11 15:45:32 +0900

    add portfolioSection

--- js/script.js
+++ js/script.js
@@ -64,4 +64,25 @@ $(document).ready(function () {
       countUpFinished = true;
     }
   });
+
+  $("[data-fancybox]").fancybox();
+
+  $("#filters a").click(function() {
+    $("#filters .current").removeClass("current");
+    $(this).addClass("current");
+
+    var selector = $(this).attr("data-filter");
+
+    $(".items").isotope({
+      filter: selector,
+      animationOption: {
+        duration: 1500,
+        easing: 'linear',
+        queue: false
+      }
+    });
+
+    return false;
+  });
+
 });
